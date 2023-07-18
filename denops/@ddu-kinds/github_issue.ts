395033a981f10bf9f6e28736528beb6bc4ee6bc9
kyoh86 <me@kyoh86.dev>
2023-07-18 09:11:06 +0900

    preview body

--- denops/@ddu-kinds/github_issue.ts
+++ denops/@ddu-kinds/github_issue.ts
@@ -1,9 +1,13 @@
 import {
   ActionFlags,
-  Actions,
   BaseKind,
 } from "https://deno.land/x/ddu_vim@v3.4.2/types.ts";
-import { Issue } from "../ddu-source-github/github/types.ts";
+import type {
+  Actions,
+  Previewer,
+} from "https://deno.land/x/ddu_vim@v3.4.2/types.ts";
+import type { Issue } from "../ddu-source-github/github/types.ts";
+import type { GetPreviewerArguments } from "https://deno.land/x/ddu_vim@v3.4.2/base/kind.ts";
 
 export type ActionData = Issue;
 
@@ -19,6 +23,22 @@ export class Kind extends BaseKind<Params> {
       return ActionFlags.None;
     },
   };
+  override getPreviewer(
+    { item }: GetPreviewerArguments,
+  ): Promise<Previewer | undefined> {
+    const action = item.action as ActionData;
+    if (action.body) {
+      return Promise.resolve({
+        kind: "nofile",
+        contents: action.body.split(/\r?\n/),
+      });
+    } else {
+      return Promise.resolve({
+        kind: "nofile",
+        contents: ["No content"],
+      });
+    }
+  }
   params(): Params {
     return {};
   }
