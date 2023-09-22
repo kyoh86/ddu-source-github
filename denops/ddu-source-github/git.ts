d7d42ba9059fd083e8340890afe9b405b066d763
GitHub <noreply@github.com>
2023-09-22 02:45:41 +0000

    chore(deps): Update deno dependencies

--- denops/ddu-source-github/git.ts
+++ denops/ddu-source-github/git.ts
@@ -1,4 +1,4 @@
-import { dirname, join } from "https://deno.land/std@0.201.0/path/mod.ts";
+import { dirname, join } from "https://deno.land/std@0.202.0/path/mod.ts";
 import { decode } from "https://deno.land/x/ini@v2.1.0/mod.ts";
 import { is, maybe } from "https://deno.land/x/unknownutil@v3.6.0/mod.ts";
 
