cfc5e614fe3184a3c8f706ff86d104f25c223b38
GitHub <noreply@github.com>
2023-11-11 09:02:06 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-sources/github_repo_issue.ts
+++ denops/@ddu-sources/github_repo_issue.ts
@@ -1,6 +1,6 @@
-import type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
+import type { Denops } from "https://deno.land/x/denops_std@v5.0.2/mod.ts";
 import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.6.0/base/source.ts";
-import { getcwd } from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
+import { getcwd } from "https://deno.land/x/denops_std@v5.0.2/function/mod.ts";
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
 import { getClient } from "../ddu-source-github/github/client.ts";
 import { gitdir, parseGitHubRepo } from "../ddu-source-github/git.ts";
--- denops/@ddu-sources/github_repo_pull.ts
+++ denops/@ddu-sources/github_repo_pull.ts
@@ -1,5 +1,5 @@
 import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.6.0/base/source.ts";
-import { getcwd } from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
+import { getcwd } from "https://deno.land/x/denops_std@v5.0.2/function/mod.ts";
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
 import { getClient } from "../ddu-source-github/github/client.ts";
 import { gitdir, parseGitHubRepo } from "../ddu-source-github/git.ts";
