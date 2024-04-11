aaaf8b0455feb50b57a4d2bafac2fa40ce134348
kyoh86 <me@kyoh86.dev>
2024-04-11 21:39:19 +0900

    Update dependencies

--- denops/ddu-source-github/git_test.ts
+++ denops/ddu-source-github/git_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.221.0/assert/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.222.1/assert/mod.ts";
 import { parseGitHubURLLike } from "./git.ts";
 
 Deno.test("parse GitHub URL #1 - undefined", () => {
