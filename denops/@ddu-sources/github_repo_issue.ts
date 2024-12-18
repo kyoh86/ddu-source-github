1bbf09f0247acd9048bd41085d64f16d95fbe138
kyoh86 <me@kyoh86.dev>
2024-12-18 17:53:25 +0900

    refactor: fetch issues

--- denops/@ddu-sources/github_repo_issue.ts
+++ denops/@ddu-sources/github_repo_issue.ts
@@ -1,14 +1,74 @@
 import type { GatherArguments } from "jsr:@shougo/ddu-vim@~9.1.0/source";
 import type { Item } from "jsr:@shougo/ddu-vim@~9.1.0/types";
 import { BaseSource } from "jsr:@shougo/ddu-vim@~9.1.0/source";
+import { is, maybe } from "jsr:@core/unknownutil@4.3";
 import { getClient } from "../ddu-source-github/github/client.ts";
 import { githubRepo, type RepoParams } from "../ddu-source-github/git.ts";
 import type { ActionData } from "../@ddu-kinds/github_issue.ts";
-import { is, maybe } from "jsr:@core/unknownutil@4.3";
 
-type Params = RepoParams & {
-  state: "open" | "closed" | "all";
+type State = "open" | "closed" | "all";
+type Params = RepoParams & { state: State };
+
+type RawLabel = string | {
+  name?: string;
 };
+
+function convertLabels(labels: RawLabel[]) {
+  return labels.map((
+    l,
+  ) => (typeof l == "string"
+    ? { name: l ?? "" }
+    : { ...l, name: l.name ?? "" })
+  );
+}
+
+const ControllerClosed =
+  "The stream controller cannot close or enqueue" as const;
+
+function maybeControlleClosed(e: unknown) {
+  const err = maybe(e, is.ObjectOf({ message: is.String }));
+  if (err && err.message === ControllerClosed) {
+    return ControllerClosed;
+  }
+  return undefined;
+}
+
+async function fetchItems(
+  owner: string,
+  name: string,
+  state: State,
+  controller: ReadableStreamDefaultController<Item<ActionData>[]>,
+) {
+  const client = await getClient();
+  const iterator = client.paginate.iterator(
+    client.rest.issues.listForRepo,
+    {
+      owner: owner,
+      repo: name,
+      per_page: 100,
+      state: state,
+    },
+  );
+
+  // iterate through each response
+  for await (const { data: issues } of iterator) {
+    try {
+      controller.enqueue(
+        issues.filter((issue) => !issue.pull_request).map((issue) => ({
+          action: { ...issue, labels: convertLabels(issue.labels) },
+          word: `${issue.number} ${issue.title}`,
+        })),
+      );
+    } catch (e) {
+      if (maybeControlleClosed(e)) {
+        console.debug(ControllerClosed);
+      } else {
+        console.warn(e);
+      }
+      break;
+    }
+  }
+}
 
 export class Source extends BaseSource<Params, ActionData> {
   override kind = "github_issue";
@@ -21,62 +81,15 @@ export class Source extends BaseSource<Params, ActionData> {
         try {
           const repo = await githubRepo(denops, sourceParams);
           if (repo === undefined) {
-            console.error(
-              `invalid param: ${JSON.stringify(sourceParams)}`,
-            );
+            console.error(`invalid param: ${JSON.stringify(sourceParams)}`);
             return;
           }
-          const client = await getClient();
-          const iterator = client.paginate.iterator(
-            client.rest.issues.listForRepo,
-            {
-              owner: repo.owner,
-              repo: repo.name,
-              per_page: 100,
-              state: sourceParams.state,
-            },
+          await fetchItems(
+            repo.owner,
+            repo.name,
+            sourceParams.state,
+            controller,
           );
-
-          // iterate through each response
-          for await (const { data: issues } of iterator) {
-            try {
-              controller.enqueue(
-                issues.filter((issue) => !issue.pull_request).map(
-                  (issue) => {
-                    const labels = issue.labels.map((
-                      l,
-                    ) => (typeof l == "string"
-                      ? { name: l ?? "" }
-                      : { ...l, name: l.name ?? "" })
-                    );
-                    return {
-                      action: {
-                        ...issue,
-                        labels,
-                      },
-                      word: `${issue.number} ${issue.title}`,
-                    };
-                  },
-                ),
-              );
-            } catch (e) {
-              const err = maybe(
-                e,
-                is.ObjectOf({
-                  message: is.String,
-                }),
-              );
-              if (
-                err &&
-                err.message === "The stream controller cannot close or enqueue"
-              ) {
-                console.log(err.message);
-              } else {
-                console.warn(e);
-              }
-              break;
-            }
-          }
         } catch (e) {
           console.error(e);
         } finally {
