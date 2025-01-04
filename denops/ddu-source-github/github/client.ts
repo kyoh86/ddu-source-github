6f14a294dfc648696e2f0d2115fe0618bcdb55ef
kyoh86 <me@kyoh86.dev>
2025-01-04 16:01:34 +0900

    Update dependencies

--- denops/ddu-source-github/github/client.ts
+++ denops/ddu-source-github/github/client.ts
@@ -1,12 +1,12 @@
 import { restoreAuthentication, storeAuthentication } from "./auth.ts";
-import { Octokit as OctokitCore } from "npm:@octokit/core@6.1.2";
+import { Octokit as OctokitCore } from "npm:@octokit/core@6.1.3";
 import { restEndpointMethods } from "npm:@octokit/plugin-rest-endpoint-methods@13.2.6";
 import { paginateRest } from "npm:@octokit/plugin-paginate-rest@11.3.6";
 
 export const Octokit = OctokitCore.plugin(restEndpointMethods).plugin(
   paginateRest,
 );
-import { createOAuthDeviceAuth } from "npm:@octokit/auth-oauth-device@7.1.1";
+import { createOAuthDeviceAuth } from "npm:@octokit/auth-oauth-device@7.1.2";
 import { systemopen } from "jsr:@lambdalisue/systemopen@~1.0.0";
 type Verification = {
   device_code: string;
