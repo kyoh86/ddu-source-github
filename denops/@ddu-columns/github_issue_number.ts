99f0372a3ea446011a9a57dd311be02054edbb25
GitHub <noreply@github.com>
2025-02-01 09:03:04 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-columns/github_issue_number.ts
+++ denops/@ddu-columns/github_issue_number.ts
@@ -1,4 +1,4 @@
-import type { ItemHighlight } from "jsr:@shougo/ddu-vim@~9.4.0/types";
+import type { ItemHighlight } from "jsr:@shougo/ddu-vim@~9.5.0/types";
 import type { ActionData } from "../@ddu-kinds/github_issue.ts";
 import { GithubBaseColumn } from "./github_base.ts";
 import type { Denops } from "jsr:@denops/std@~7.4.0";
