64fffc5b04799dd5adc17bd388d5c0ebe5c6e6b1
GitHub <noreply@github.com>
2025-05-25 16:08:14 +0000

    chore(deps): Update deno dependencies

--- spec/matcher_github_issue_like_test.ts
+++ spec/matcher_github_issue_like_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "jsr:@std/assert@1.0.1";
+import { assertEquals } from "jsr:@std/assert@1.0.13";
 import { parseQuery } from "../denops/@ddu-filters/matcher_github_issue_like.ts";
 
 Deno.test("parse query", () => {
