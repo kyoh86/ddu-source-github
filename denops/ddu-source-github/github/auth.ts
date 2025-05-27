6d83bc0926800d55ddd3554b16641daaf26c5366
GitHub <noreply@github.com>
2025-05-27 09:03:27 +0000

    chore(deps): Update deno dependencies

--- denops/ddu-source-github/github/auth.ts
+++ denops/ddu-source-github/github/auth.ts
@@ -1,5 +1,5 @@
 import xdg from "https://deno.land/x/xdg@v10.6.0/src/mod.deno.ts";
-import { join } from "jsr:@std/path@1.0.9";
+import { join } from "jsr:@std/path@1.1.0";
 import { ensureDir } from "jsr:@std/fs@1.0.17";
 import type {
   GitHubAppAuthentication,
